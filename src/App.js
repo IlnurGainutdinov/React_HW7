d636eb9d7dfbdeaa7456377e4aa33ac3aa83f9f0
IlnurGainutdinov <gainutdinov86@list.ru>
2024-10-17 20:38:51 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { fetchUsers } from "./store/usersReduser";
+import { fetchUser } from "./store/userReduser";
+import "./styles/style.scss";
 
 function App() {
+  const { users, loading, error } = useSelector((state) => state.users);
+  const { user, loadingUser, errorUser } = useSelector((state) => state.user);
+  const [userId, setUserId] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
+  const handleClick = (userId) => {
+    setUserId(userId);
+    dispatch(fetchUser(userId));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="block">
+      <h1 className="block__title">Пользователи</h1>
+      {loading && <p>Загрузка...</p>}
+      {error && <p>Ошибка {error}</p>}
+      {users.length ? (
+        <ul>
+          {users.map((person) => (
+            <details className="block__details" key={person.id} onClick={() => handleClick(person.id)}>
+              <summary className="block__summary">{person.name}</summary>
+              {loadingUser && <p>Загрузка...</p>}
+              {errorUser && <p>Ошибка {errorUser}</p>}
+              {userId === person.id ? (
+                <div className="block__user">
+                  <p>User: {user.username}</p>
+                  <p>e-mail: {user.email}</p>
+                </div>
+              ) : null}
+            </details>
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 }
